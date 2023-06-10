2d0f7522f54d268db321a5567ed0890c51290436
Patryk Karpinski <patrykarpinski96@gmail.com>
2023-06-10 20:02:32 +0200

    create home page

---
+++ lib/features/page/city_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CityPage extends StatefulWidget {
+  const CityPage({super.key});
+
+  @override
+  State<CityPage> createState() => _CityPageState();
+}
+
+class _CityPageState extends State<CityPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
